c498a8d90ec11cd0cb6aa41d810c570d4592006c
suthakaranburaj <suthakaranburaj@gmail.com>
2024-11-06 02:47:39 +0530

    small change

--- client/src/Pages/HomePage/ProductSection/ProductSection.jsx
+++ client/src/Pages/HomePage/ProductSection/ProductSection.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 import './ProductSection.scss'
 import { Link } from 'react-router-dom'
+import userAuthStore from '../../../store/useAuth.store'
 
 function ProductSection({id, title, price, image}) {
+    const {user,isAuthenticated} = userAuthStore();
     return (
-        <Link key={id} to={`/product/${id}`}>
+        <Link key={id} to={isAuthenticated ? `/product/${id}` : `/signup`}>
             <div  className='productSectionContainer'>
                 <div className='productSectionContainer1'>
                     <img className='productSectionContainer11' src={image} alt="" />
